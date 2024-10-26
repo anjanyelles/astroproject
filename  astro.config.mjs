e323a90df9e672b77121f30ee16c0c2bd50eeb3f
anjanyelle <anjanyelle@gmail.com>
2024-10-26 15:29:37 +0530

    ch

---  astro.config.mjs
+++  astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
-  site: 'https://astroproject-4637hho7p-anjans-projects-2103206a.vercel.app', // Update to your production URL
+  integrations: [react(), tailwind()],
+  site: "https://astroproject-4637hho7p-anjans-projects-2103206a.vercel.app", // Update this to your production URL
+  base: "/", // Adjust this if necessary
 });
